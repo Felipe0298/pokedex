2f8ca525ef8839980ba2836bbbd8f2470c64ba66
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-13 11:54:06 -0500

    271. useDebounceValue - CustomHook

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, View, TextInput, Platform, StyleProp, ViewStyle } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useDebouncedValue } from '../hooks/useDebouncedValue';
 
 interface Props {
   style?: StyleProp<ViewStyle>
 }
 
 export const SearchInput = ({style} : Props ) => {
+
+  const [textValue, setTextValue] = useState('');
+
+  const debouncedValue = useDebouncedValue( textValue)
+
+  useEffect(() => {
+   console.log({debouncedValue})
+  }, [debouncedValue])
+  
+
   return (
     <View style={{...styles.container,
       ...style as any
@@ -20,6 +31,8 @@ export const SearchInput = ({style} : Props ) => {
           }}
           autoCapitalize='none'
           autoCorrect={false}
+          value={textValue}
+          onChangeText={setTextValue}
         />
 
         <Icon
