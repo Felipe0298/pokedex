f34dfced073352988d9747da3f5abf4e1eea04c6
Felipe Hoyos Taborda <felipe02-98@hotmail.com>
2023-10-13 08:43:24 -0500

    267. Implementar Tabs en nuestra aplicación

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import { Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
+import { Tabs } from './src/navigator/Tabs';
 
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator/>
+      {/* <Navigator/> */}
+      <Tabs/>
     </NavigationContainer>
   );
 }
---
+++ src/navigator/Tabs.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Navigator } from './Navigator';
+import { SearchScreen } from '../screens/SearchScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+export const Tabs = () => {
+  return (
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white'
+      }}
+      screenOptions={{
+        tabBarActiveTintColor: '#5856D5',
+        tabBarStyle: {
+          position: 'absolute',
+          backgroundColor: 'rgba(255,255,255,0.82)',
+          paddingBottom: 10, //( Platform.OS === 'ios') ? 0 : 10, 
+          borderWidth: 0,
+          elevation: 0,
+          height: 60,//( Platform.OS === 'ios') ? 70 : 80,
+        }
+      }}
+    >
+      <Tab.Screen name="HomeScreen" component={Navigator}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({ color }) => (
+            <Icon color={color}
+              size={20}
+              name='list-outline'
+            />
+          )
+        }}
+      />
+      <Tab.Screen name="SearchScreen" component={SearchScreen}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({ color }) => (
+            <Icon color={color}
+              size={20}
+              name='list-outline'
+            />
+          )
+        }} />
+    </Tab.Navigator>
+  );
+}
